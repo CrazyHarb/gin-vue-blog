4e367fab30e8631c9e039ecc9235e808c0a94dcc
hongxuebo <hongxuebo@bytedance.com>
2023-12-12 20:26:49 +0800

    add upload loginfo.

--- gin-blog-server/service/file.go
+++ gin-blog-server/service/file.go
@@ -3,6 +3,7 @@ package service
 import (
 	"gin-blog/utils/r"
 	"gin-blog/utils/upload"
+	"log"
 	"mime/multipart"
 )
 
@@ -13,6 +14,7 @@ func (*File) UploadFile(header *multipart.FileHeader) (url string, code int) {
 	oss := upload.NewOSS()
 	filePath, _, uploadErr := oss.UploadFile(header)
 	if uploadErr != nil {
+		log.Printf("[upload]error: %s", uploadErr.Error())
 		return "", r.ERROR_FILE_UPLOAD
 	}
 	return filePath, r.OK
